49c8cef23b77e45208a86770dfe4d5759a10e966
susehan <susannahan27@gmail.com>
2018-10-06 20:53:08 -0400

    files

--- nytreact/src/App.js
+++ nytreact/src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/saved" component={Saved}/>
-    </div>
-  </Router>
-)
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h1 className="display-4">NYT SEARCH</h1>
+                <p className="lead">the react ver.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">Search</div>
+                <div className="form-group">
+                  <label>Topic</label>
+                  <input className="form-control"></input>
+                </div>
+                <div className="form-group">
+                  <label>Start Year</label>
+                  <input className="form-control"></input>
+                </div>
+                <div className="form-group">
+                  <label>End Year</label>
+                  <input className="form-control"></input>
+                </div>
+                <button type="submit" className="btn btn-light">Search</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+              <div className="card-title">Result</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
